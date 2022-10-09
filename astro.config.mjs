3c08931e7caeb03cde783e7865bd4955f7eb88ac
t0yohei <inuneko690121@gmail.co>
2022-10-09 17:43:39 +0900

    add netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import vue from "@astrojs/vue";
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue()],
-  site: "https://t0yohei.github.io/",
-  base: "astro-sample",
+  // site: "https://t0yohei.github.io/",
+  // base: "astro-sample",
 });
